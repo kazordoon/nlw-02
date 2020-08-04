50c3434d39ce78111d10701c5c50540ebf0e9a80
Felipe Barros <felipetech@tuta.io>
2020-08-04 17:26:32 -0300

    chore(web): Add the initial structure of the teacher form page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
 
 const TeacherForm = () => {
   return (
-    <h1>Teacher Form</h1>
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas." />
+      <h1>Teacher Form</h1>
+    </div>
   );
 };
 
