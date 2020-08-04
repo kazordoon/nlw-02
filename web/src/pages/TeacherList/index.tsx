31d82d94b3707179a686963deb05f2babec13102
Felipe Barros <felipetech@tuta.io>
2020-08-04 17:25:00 -0300

    chore(web): Add the teacher list page

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+import TeacherItem from '../../components/TeacherItem';
 
 const TeacherList = () => {
   return (
-    <h1>Teachers</h1>
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponívels.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="week-day">Dia da semana</label>
+            <input type="text" id="week-day" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="time">Horário</label>
+            <input type="text" id="time" />
+          </div>
+        </form>
+      </PageHeader>
+
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
+    </div>
   );
 };
 
