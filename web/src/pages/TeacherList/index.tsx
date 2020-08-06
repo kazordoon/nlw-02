0747a296142d75752a348028261b4917158dc5a9
Felipe Barros <felipetech@tuta.io>
2020-08-06 11:20:52 -0300

    refactor(web): Use the select component instead the input component

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,17 +1,46 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
-import './styles.css';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
+
+import './styles.css';
 
 const TeacherList = () => {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponívels.">
         <form id="search-teachers">
-          <Input id="subject" label="Matéria" />
-          <Input id="wek-day" label="Dia da semana" />
+           <Select
+            id="subject"
+            label="Matéria"
+            options={[
+              { value: 'artes', label: 'Artes' },
+              { value: 'biologia', label: 'Biologia' },
+              { value: 'ciencias', label: 'Ciências' },
+              { value: 'physical_education', label: 'Educação Física' },
+              { value: 'physics', label: 'Física' },
+              { value: 'geography', label: 'Geografia' },
+              { value: 'history', label: 'História' },
+              { value: 'math', label: 'Matemática' },
+              { value: 'portuguese', label: 'Português' },
+              { value: 'chemistry', label: 'Química' },
+            ]}
+          />
+          <Select
+            id="week_day"
+            label="Dia da Semana"
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sábado' },
+            ]}
+          />
           <Input type="time" id="time" label="Horário" />
         </form>
       </PageHeader>
