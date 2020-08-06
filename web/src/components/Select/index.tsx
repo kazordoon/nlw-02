abb92ffbc2da04b8e391ed9d36629c49f60e3835
Felipe Barros <felipetech@tuta.io>
2020-08-06 13:08:28 -0300

    refactor(web): Update the select component
    Use the property "value" instead "defaultValue"

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -8,8 +8,8 @@ const Select: React.FC<SelectProps> = ({ label, id, options, ...attributes }) =>
   return (
     <div className="select-block">
       <label htmlFor={id}>{label}</label>
-      <select id={id} {...attributes} defaultValue="default">
-        <option disabled hidden value="default">Selecione uma opção</option>
+      <select id={id} {...attributes} value="">
+        <option disabled hidden value="">Selecione uma opção</option>
         {options.map((option) => (
           <option key={option.value} value={option.value}>{option.label}</option>
         ))}
