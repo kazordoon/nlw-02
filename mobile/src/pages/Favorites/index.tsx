426718bed2a460b33e6caa8dfd0e04e1640b00e1
Felipe Barros <felipetech@tuta.io>
2020-08-07 12:28:45 -0300

    chore(mobile): Add the structure of the favorites page

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './styles';
+
+const Favorites = () => {
+  return (
+    <View></View>
+  );
+};
+
+export default Favorites;
