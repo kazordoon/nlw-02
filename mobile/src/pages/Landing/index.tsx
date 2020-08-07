5366f993d70a008496f1cdcd93cc549db6ceb458
Felipe Barros <felipetech@tuta.io>
2020-08-07 11:51:42 -0300

    chore(mobile): Add navigation to the give classes page on the landing page

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import LandingImage from '../../assets/images/landing.png';
 import StudyIcon from '../../assets/images/icons/study.png';
@@ -9,6 +11,12 @@ import GiveClassesIcon from '../../assets/images/icons/give-classes.png';
 import styles from './styles';
 
 const Landing = () => {
+  const { navigate } = useNavigation();
+
+  function handleNavigationToTheGiveClassesPage() {
+    navigate('GiveClasses')
+  }
+
   return (
     <View style={styles.container}>
       <Image source={LandingImage} style={styles.banner} />
@@ -18,15 +26,18 @@ const Landing = () => {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+        <RectButton style={[styles.button, styles.buttonPrimary]}>
           <Image source={StudyIcon} />
           <Text style={styles.buttonText}>Estudar</Text>
-        </TouchableOpacity>
+        </RectButton>
 
-        <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+        <RectButton
+          onPress={handleNavigationToTheGiveClassesPage}
+          style={[styles.button, styles.buttonSecondary]}
+        >
           <Image source={GiveClassesIcon} />
           <Text style={styles.buttonText}>Dar aulas</Text>
-        </TouchableOpacity>
+        </RectButton>
       </View>
 
       <Text style={styles.totalConnections}>
