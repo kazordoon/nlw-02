5dd3e133289943d9ec502a04030df34e92463907
Felipe Barros <felipetech@tuta.io>
2020-08-08 11:33:27 -0300

    chore(mobile): Add the connections count

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
@@ -9,16 +9,37 @@ import heartIcon from '../../assets/images/icons/heart.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 
 import styles from './styles';
+import api from '../../services/api';
 
 const Landing = () => {
   const { navigate } = useNavigation();
 
+  const [totalConnections, setTotalConnections] = useState(0);
+  const [connectionPhrase, setConnectionPhrase] = useState(
+    `Total de ${totalConnections} conexões já realizadas.  `
+  );
+
+  useEffect(() => {
+    api.get('connections').then((response) => {
+      const total = Number(response.data.total);
+      setTotalConnections(total);
+
+      if (total === 1) {
+        setConnectionPhrase('Total de 1 conexão já realizada.  ');
+      } else {
+        setConnectionPhrase(
+          `Total de ${totalConnections} conexões já realizadas.  `
+        );
+      }
+    });
+  });
+
   function handleNavigationToTheGiveClassesPage() {
-    navigate('GiveClasses')
+    navigate('GiveClasses');
   }
 
   function handleNavigationToTheStudyPages() {
-    navigate('Study')
+    navigate('Study');
   }
 
   return (
@@ -48,7 +69,7 @@ const Landing = () => {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 200 conexões já realizadas.{' '}
+        {connectionPhrase}
         <Image source={heartIcon} />
       </Text>
     </View>
